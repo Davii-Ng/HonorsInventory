f01372e7923f38871c7058b3623afe8e9562a92d
Davii-Ng <nminhd98@gmail.com>
2025-11-27 00:15:53 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <button onClick = {() => setCount(count + 1)}></button>
+    {count}
+    </>
+  )
+}
+
+export default App
