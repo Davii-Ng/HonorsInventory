b9cb46fe2df15dfc616aecbafec8942e3f57c50c
Davii-Ng <nminhd98@gmail.com>
2025-11-30 16:53:50 -0500

    prototype function runs

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,25 @@
 import express from "express";
-const app = express()
+import { supabase } from "./lib/supabase.js";
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
-app.get('/', (request, response) =>{
-    response.send('<h1>Hello world</h1>')
-})
+const app = express();
+app.use(express.json());
 
-app.get('/api/notes', (request, response) =>{
-    response.json(notes)
-})
+
+
+app.get('/', async (request, response) =>{
+    const {data, error} = await supabase
+    .from('equipment')
+    .select()
+
+    response.json(data)
+});
+
+app.get('/api/data', (request, response) =>{
+    response.json(data)
+});
 
 const PORT = 3001
 app.listen(PORT, () =>{
     console.log(`Server running on ${PORT}`)
-})
+});
