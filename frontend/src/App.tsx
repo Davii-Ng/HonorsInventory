6aff4239d00c4fae26b3f5ac7dfbb6f03d98f768
Davii-Ng <nminhd98@gmail.com>
2025-12-01 16:02:50 -0500

    seem to working update button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,23 +10,42 @@ interface Equipment {
 
 export default function App(){
   const [item, setItem] = useState<Equipment []>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [editItem,setEditItem] = useState<Equipment|null>(null);
+ 
+  
 
-
-  // Load data
-  useEffect(() => {
-    const load = async () => {
+  const load = async () => {
       try{
         const res = await fetch(`${import.meta.env.VITE_API_URL}/api/equipment`)
         console.log('res', res)
+
         const data: Equipment[] = await res.json()
         console.log('Data: ', data)
+
         setItem(data)
       } catch (err){
         console.log("Failed to fetch", err);
       }
     };
+
+  // Load data
+  useEffect(() => {
     load()
   }, []);
+
+  
+  const handleSave = async() =>{
+    await fetch(`${import.meta.env.VITE_API_URL}/api/equipment/${editItem?.id}`,{
+      method: "PUT",
+      headers : {"Content-Type": "application/json"},
+      body: JSON.stringify(editItem)
+    });
+
+
+    setIsModalOpen(false);
+    load();
+  };
 
 
   async function handleDelete(id: number){
@@ -61,18 +80,59 @@ export default function App(){
         </tr>
       </thead>
       <tbody>
-        {item.map((item) => ( 
-          <tr key = {item.id}>
-            <th>{item.id}</th>
-            <th>{item.model}</th>
-            <th>{item.equipment_type}</th>
-            <th>{item.location}</th>
-            <td><button>Edit</button></td>
-            <td><button onClick={() =>{handleDelete(item.id)}} >Delete</button></td>
+        {item.map((equipment) => ( 
+          <tr key = {equipment.id}>
+            <th>{equipment.id}</th>
+            <th>{equipment.model}</th>
+            <th>{equipment.equipment_type}</th>
+            <th>{equipment.location}</th>
+            <td><button onClick = {() => {
+              setEditItem(equipment);  
+              setIsModalOpen(true);
+              }}>Edit</button></td>
+            <td><button onClick={() =>{handleDelete(equipment.id)}} >Delete</button></td>
           </tr>
         ))}
       </tbody>
     </table>
+    {isModalOpen && (
+      <div>
+        <div>
+          <h2>Edit Equipment</h2>
+
+
+          <input
+          type = "text"
+          value = {editItem?.model}
+          onChange = {(e) =>
+            setEditItem({...editItem, model: e.target.value})
+          }
+          ></input>
+
+          <input
+          type = "text"
+          value = {editItem?.equipment_type}
+          onChange = {(e) =>
+            setEditItem({...editItem, equipment_type: e.target.value})
+          }
+          ></input>
+
+          <input
+          type = "text"
+          value = {editItem?.location}
+          onChange = {(e) =>
+            setEditItem({...editItem, location: e.target.value})
+          }
+          ></input>
+
+          <button onClick = {handleSave}>Save</button>
+          <button onClick = {() => setIsModalOpen(false)}>Cancel</button>
+        </div>
+      </div>
+    )}
+
+    
+
     <button> Add Equipment</button>
   </div>
 )
